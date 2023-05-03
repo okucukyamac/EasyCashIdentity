4650f54f0a726338ede9ce57c3da61f87c3489e2
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-05-03 23:15:22 +0300

    cmt

---
+++ EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public string BankBranch{ get; set; }
+    }
+}
