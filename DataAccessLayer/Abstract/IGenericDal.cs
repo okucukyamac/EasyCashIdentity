6bae77bd52244d0818fd51e531af2cc7e7870ae4
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-05-08 22:27:43 +0300

    generic repository Igenericdal

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using (var context = new Context()) 
+            {
+                context.Set<T>().Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            using (Context context = new Context())
+            {
+                return context.Set<T>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            using (var context = new Context())
+            {
+                return filter == null ? context.Set<T>().ToList() : context.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public T GetById(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<T>().Find(id);
+            }
+        }
+
+        public void Insert(T entity)
+        {
+            using (var context = new Context())
+            {
+                context.Set<T>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (var context = new Context())
+            {
+                context.Set<T>().Update(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
